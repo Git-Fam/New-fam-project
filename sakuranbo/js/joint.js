ced03746c12e396f2f1c5da046e12406dc2e0fa0
kaku <ikumi.kaku818@gmail.com>
2024-05-17 03:55:39 +0900

    no message

--- sakuranbo/js/joint.js
+++ sakuranbo/js/joint.js
@@ -26,5 +26,17 @@ $(function () {
   //   });
   // });
 
+  // はねる見出し
+  $(window).scroll(function () {
+    $('.bounce').each(function () {
+      var top_of_element = $(this).offset().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if (bottom_of_window > top_of_element) {
+        $(this).addClass('show');
+      }
+    });
+  });
+
+
 
 });
