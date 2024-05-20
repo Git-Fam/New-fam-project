e962d340e32597513cd0e74e25cac798928ddd62
手老隆貴 <seedless0802@gmail.com>
2024-05-20 10:29:01 +0900

    結合.Ver

--- sakuranbo/js/script.js
+++ sakuranbo/js/script.js
@@ -44,20 +44,10 @@ $(function () {
     $('.menu__bg').removeClass("active");
   });
 
-  // var prevScrollpos = window.pageYOffset;
-  // window.onscroll = function() {
-  //   var currentScrollpos = window.pageYOffset;
-  //   if (prevScrollpos > currentScrollpos || currentScrollpos < 450) {
-  //     document.querySelector(".header").classList.remove("active");
-  //   } else {
-  //     document.querySelector(".header").classList.add("active");
-  //   }
-  //   prevScrollpos = currentScrollpos;
-  // }
 
   // 要素が画面下部に来たらshowを付与
   $(window).scroll(function () {
-    $('.up,.down,.left,.right,.pop,.juwa,.graphScale,.bounce,.stamp').each(function () {
+    $('.up,.down,.left,.right,.pop,.juwa,.graphScale,.bounce,.stamp,.flow_juwa').each(function () {
       var top_of_element = $(this).offset().top;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       if (bottom_of_window > top_of_element) {
@@ -65,22 +55,5 @@ $(function () {
       }
     });
   });
-
-
-  // ローディング
-  // var loadingFinished = false;
-  // var loading = $('.loadUp');
-
-  // $(window).on('load', function () {
-  //   loading.addClass('show');
-  //   loadingFinished = true;
-  // });
-  // setTimeout(function(){
-  //   if (!loadingFinished) {
-  //     loading.addClass('show');
-  //   }
-  // }, 2000);
-
-
 
 });
