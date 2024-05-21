3176c6be8f14d5037a1b105c4059da65d51a5c3e
手老隆貴 <seedless0802@gmail.com>
2024-05-22 01:06:55 +0900

    no message

--- daiwa/js/joint.js
+++ daiwa/js/joint.js
@@ -10,4 +10,22 @@ $(function () {
     $('body').removeClass("active");
   }); 
 
+
+    // 要素が画面下部に来たらshowを付与
+    $(window).scroll(function () {
+      $('.bon').each(function () {
+        var top_of_element = $(this).offset().top;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > top_of_element) {
+          $(this).addClass('show');
+        }
+      });
+    });
+
+    $(".btn__area .TX").hover(function(){
+      $(this).toggleClass("active");
+      $(".btn__area .hr").toggleClass("active");
+      $(".C_ServiceBtn .circle").toggleClass("active");
+    });
+
 });
