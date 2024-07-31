0ade2089440f40fc5bb85b4e9db7e7b7ce198e6b
kaku <ikumi.kaku818@gmail.com>
2024-07-31 14:24:07 +0900

    no message

--- pequod_HP/js/joint.js
+++ pequod_HP/js/joint.js
@@ -1,5 +1,30 @@
-$(function () {   
-
-
-
-});
+// C_TL
+document.querySelectorAll(".C_TL").forEach((container) => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            const items = entry.target.querySelectorAll(".js-g01");
+            items.forEach((item, i) => {
+              gsap.fromTo(item, {
+                opacity: 0,
+                scale: 2,
+                yPercent: -75
+              }, {
+                opacity: 1,
+                scale: 1,
+                yPercent: 0,
+                ease: "power1",
+                duration: 0.5,
+                delay: i * 0.05
+              });
+            });
+            observer.unobserve(entry.target); // アニメーション発火後に監視を解除
+          }, 800); // 0.8秒後に発火
+        }
+      });
+    }, { threshold: 0.1 });
+  
+    observer.observe(container); // 各C_TL要素を監視
+  });
+  
