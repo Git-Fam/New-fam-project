eae50dcd75f70e7fcf8af177c49e3a6f529a216c
kaku <ikumi.kaku818@gmail.com>
2024-09-03 15:33:22 +0900

    no message

--- In-House-Curriculum/function/common/post.php
+++ In-House-Curriculum/function/common/post.php
@@ -105,6 +105,97 @@ function create_post_type()
       'update_count_callback' => '_update_post_term_count',
     )
   );
+
+
+  // カスタム投稿タイプ「講師への質問」
+  register_post_type(
+    'question',
+    array(
+      'label' => '講師への質問',
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'question'), // スラッグを設定
+      'menu_position' => 7,
+      'show_in_rest' => true,
+      'supports' => array(
+        'title',
+        'editor',
+        'thumbnail',
+        'revisions'
+      ),
+    )
+  );
+
+  // カスタム投稿タイプ「講師への質問」のカテゴリー
+  register_taxonomy(
+    'question-cat',
+    'question',
+    array(
+      'label' => 'カテゴリー',
+      'hierarchical' => true,
+      'public' => true,
+      'show_in_rest' => true,
+      'rewrite' => array('slug' => 'question-cat'), // スラッグを設定
+    )
+  );
+
+  // カスタム投稿タイプ「講師への質問」のタグ
+  register_taxonomy(
+    'question-tag',
+    'question',
+    array(
+      'label' => 'タグ',
+      'hierarchical' => false,
+      'public' => true,
+      'show_in_rest' => true,
+      'update_count_callback' => '_update_post_term_count',
+    )
+  );
+
+    // カスタム投稿タイプ「ミニゲーム」
+  register_post_type(
+    'game',
+    array(
+      'label' => 'ミニゲーム',
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'game'), // スラッグを設定
+      'menu_position' => 8,
+      'show_in_rest' => true,
+      'supports' => array(
+        'title',
+        'editor',
+        'thumbnail',
+        'revisions'
+      ),
+    )
+  );
+
+  // カスタム投稿タイプ「ミニゲーム」のカテゴリー
+  register_taxonomy(
+    'game-cat',
+    'game',
+    array(
+      'label' => 'カテゴリー',
+      'hierarchical' => true,
+      'public' => true,
+      'show_in_rest' => true,
+      'rewrite' => array('slug' => 'game-cat'), // スラッグを設定
+    )
+  );
+
+  // カスタム投稿タイプ「ミニゲーム」のタグ
+  register_taxonomy(
+    'game-tag',
+    'game',
+    array(
+      'label' => 'タグ',
+      'hierarchical' => false,
+      'public' => true,
+      'show_in_rest' => true,
+      'update_count_callback' => '_update_post_term_count',
+    )
+  );
 }
 
 add_theme_support('post-thumbnails');
