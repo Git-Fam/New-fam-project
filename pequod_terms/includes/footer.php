3f7286ddf50f6bcbb28ff708387da83ef643343a
kaku <ikumi.kaku818@gmail.com>
2024-09-20 16:33:39 +0900

    no message

--- pequod_terms/includes/footer.php
+++ pequod_terms/includes/footer.php
@@ -1,1 +1,37 @@
-<footer class="footer">footer</footer>
+<footer class="footer">
+	<nav class="nav">
+		<ul class="nav-wrap">
+			<li class="nav-item">
+				<a href="#" class="label">ホーム</a>
+				<ul class="link-list">
+					<li><a href="#">HOZON</a></li>
+					<li><a href="#">SEITON</a></li>
+					<li><a href="#">メディカルレスキュー</a></li>
+					<li><a href="#">ライフレスキュー</a></li>
+				</ul>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="label">Q&A</a>
+				<ul class="link-list">
+					<li><a href="#">◯◯について</a></li>
+					<li><a href="#">◯◯について</a></li>
+				</ul>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="label">会社概要</a>
+				<ul class="link-list">
+					<li><a href="#">事業概要</a></li>
+					<li><a href="#">アクセス</a></li>
+				</ul>
+			</li>
+			<li class="nav-item">
+				<a class="label" href="#">プライバシーポリシー</a>
+			</li>
+		</ul>
+	</nav>
+
+	<a href="#" class="logo"></a>
+	<p class="copyright">＠Copyright.waterwater</p>
+
+
+</footer>
--- pequod_terms/includes/header.php
+++ pequod_terms/includes/header.php
@@ -1,1 +1,38 @@
-<header class="header">header</header>
+<header class="header">
+    <h1 class="logo">
+        <a href="#"></a>
+    </h1>
+    <nav class="nav nav-btn">
+        <ul class="nav-wrap">
+            <li class="nav-item">
+                <p class="label">ホーム</p>
+                <ul class="link-list">
+                    <li class="link"><a href="#">HOZON</a></li>
+                    <li class="link"><a href="#">SEITON</a></li>
+                    <li class="link"><a href="#">メディカルレスキュー</a></li>
+                    <li class="link"><a href="#">ライフレスキュー</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <p class="label">Q&A</p>
+                <ul class="link-list">
+                    <li class="link"><a href="#">◯◯について</a></li>
+                    <li class="link"><a href="#">◯◯について</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <p class="label">会社概要</p>
+                <ul class="link-list">
+                    <li class="link"><a href="#">事業概要</a></li>
+                    <li class="link"><a href="#">アクセス</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="label" href="#">プライバシーポリシー</a>
+                <ul class="link-list">
+                    <li class="pc link"><a href="#">プライバシーポリシー</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
