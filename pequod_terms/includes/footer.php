1e540617a2e4e5ec72f8a3269a6ca9db5247dcf7
手老隆貴 <seedless0802@gmail.com>
2024-09-20 16:31:33 +0900

    no message

--- pequod_terms/includes/footer.php
+++ pequod_terms/includes/footer.php
@@ -1,1 +1,37 @@
-<footer class="footer">footer</footer>
+<footer class="footer">
+  <nav class="nav">
+    <ul class="nav-wrap">
+      <li class="nav-item">
+        <p class="label">ホーム</p>
+        <ul class="link-list">
+          <li><a href="#">HOZON</a></li>
+          <li><a href="#">SEITON</a></li>
+          <li><a href="#">メディカルレスキュー</a></li>
+          <li><a href="#">ライフレスキュー</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <p class="label">Q&A</p>
+        <ul class="link-list">
+          <li><a href="#">◯◯について</a></li>
+          <li><a href="#">◯◯について</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <p class="label">会社概要</p>
+        <ul class="link-list">
+          <li><a href="#">事業概要</a></li>
+          <li><a href="#">アクセス</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="label" href="#">プライバシーポリシー</a>
+      </li>
+    </ul>
+  </nav>
+
+  <div class="logo"></div>
+  <p class="copyright">＠Copyright.waterwater</p>
+
+
+</footer>
