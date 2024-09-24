df50108138c6e9ac5db4a8148ddb13824e527dc1
手老隆貴 <seedless0802@gmail.com>
2024-09-24 18:00:16 +0900

    no message

--- pequod_terms/js/script.js
+++ pequod_terms/js/script.js
@@ -1,60 +1,30 @@
-$(function () {   
-  // // ハンバーガーメニュー
-  //  $(".burger").on("click", function(){
-  //    $(this).toggleClass("active");
-  //    $('.menu').toggleClass("active");
-  //    $('body').toggleClass("active");
-  //  }); 
-  // $(".js-link,.menu").on("click", function(){
-  //   $('.burger').removeClass("active");
-  //   $('.menu').removeClass("active");
-  //   $('body').removeClass("active");
-  // });
-
-  // var prevScrollpos = window.pageYOffset;
-  // window.onscroll = function() {
-  //   var currentScrollpos = window.pageYOffset;
-  //   if (prevScrollpos > currentScrollpos || currentScrollpos < 450) {
-  //     document.querySelector(".header").classList.remove("active");
-  //   } else {
-  //     document.querySelector(".header").classList.add("active");
-  //   }
-  //   prevScrollpos = currentScrollpos;
-  // }
-
-  // 要素が画面下部に来たらshowを付与
-  // $(window).scroll(function () {
-  //   $('.up,.down,.right,.left,.pop').each(function () {
-  //     var top_of_element = $(this).offset().top;
-  //     var bottom_of_window = $(window).scrollTop() + $(window).height();
-  //     if (bottom_of_window > top_of_element) {
-  //       $(this).addClass('show');
-  //     }
-  //   });
-  // });
-
-
-  // ローディング
-  // var loadingFinished = false;
-  // var loading = $('.loadUp,.loadDown,.loadRight,.loadLeft,.loadPop');
-
-  // $(window).on('load', function () {
-  //   loading.addClass('show');
-  //   loadingFinished = true;
-  // });
-  // setTimeout(function(){
-  //   if (!loadingFinished) {
-  //     loading.addClass('show');
-  //   }
-  // }, 2000);
-
-
-  // アコーディオン
-  // $('.js_onClick').on('click', function () {
-  //   $(this).next().slideToggle();
-  //   $(this).toggleClass('active');
-  // });
+$(function () {
 
 
 
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  function updateActiveLink() {
+    var currentHash = window.location.hash;
+
+    document.querySelectorAll('.C_linkIn--item').forEach(function (link) {
+      link.classList.remove('active');
+    });
+
+    if (currentHash) {
+      var activeLink = document.querySelector('a[href="' + currentHash + '"]');
+      if (activeLink) {
+        activeLink.classList.add('active');
+      }
+    } else {
+      document.querySelector('.C_linkIn--item').classList.remove('active');
+    }
+  }
+
+  // 初期ロード時に実行
+  updateActiveLink();
+
+  // ハッシュが変更されたときに実行
+  window.addEventListener('hashchange', updateActiveLink);
 });
