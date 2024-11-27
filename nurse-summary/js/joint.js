09c623b88c8f0521c358da71b72defd5b969833f
kaku <ikumi.kaku818@gmail.com>
2024-11-28 02:04:08 +0900

    no message

--- nurse-summary/js/joint.js
+++ nurse-summary/js/joint.js
@@ -1,4 +1,11 @@
-$(function () {   
-
-
+$(function () {
+	$("#tab-btn li").on("click", function () {
+		let index = $("#tab-btn li").index(this);
+		$("li").removeClass("active");
+		$(this).addClass("active");
+		$(".C_compare-tab-content table").hide();
+		$(".C_compare-tab-content table").eq(index).show();
+		return false;
+	});
 });
+
