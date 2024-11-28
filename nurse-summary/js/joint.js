e2ee5ae7bcf262fae89a5102f8c0b450bcf81a85
kaku <ikumi.kaku818@gmail.com>
2024-11-28 10:50:19 +0900

    no message

--- nurse-summary/js/joint.js
+++ nurse-summary/js/joint.js
@@ -7,5 +7,28 @@ $(function () {
 		$(".C_compare-tab-content table").eq(index).show();
 		return false;
 	});
-});
 
+	function updateRandomPeopleCount() {
+		// 95から630の間でランダムな数を生成
+		var min = 95;
+		var max = 130;
+		var randomPeopleCount = Math.floor(Math.random() * (max - min + 1)) + min;
+
+		var today = new Date().toISOString().split("T")[0];
+
+		localStorage.setItem("randomPeopleCount", randomPeopleCount);
+		localStorage.setItem("randomPeopleDate", today);
+
+		$(".counter-container .random-counter").text(randomPeopleCount);
+	}
+
+	var storedCount = localStorage.getItem("randomPeopleCount");
+	var storedDate = localStorage.getItem("randomPeopleDate");
+	var today = new Date().toISOString().split("T")[0];
+
+	if (storedCount !== null && storedDate === today) {
+		$(".counter-container .random-counter").text(storedCount);
+	} else {
+		updateRandomPeopleCount();
+	}
+});
