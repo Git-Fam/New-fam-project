5b23ece562bdcaabbb2ec6e86dd3d035f57369e3
kaku <ikumi.kaku818@gmail.com>
2024-11-28 16:04:55 +0900

    no message

--- nurse-summary/pages/page-company.php
+++ nurse-summary/pages/page-company.php
@@ -23,76 +23,17 @@ Template Path: pages/
 
         <div class="C_terms-content company">
           <ul class="list">
-            <li class="item">
-              <p class="label">運営者</p>
-              <p class="TX">インターグ株式会社</p>
-            </li>
-            <li class="item">
-              <p class="label">住所</p>
-              <p class="TX">東京都港区六本木6-2-5 Bizflex六本木8階</p>
-            </li>
-            <li class="item">
-              <p class="label">電話番号</p>
-              <p class="TX">インターグ株式会社</p>
-            </li>
-            <li class="item">
-              <p class="label">会社ホームページ</p>
-              <p class="TX">https://</p>
-            </li>
-            <li class="item">
-              <p class="label">事業内容</p>
-              <p class="TX">
-                広告運用代行事業／グロースハック事業
-                <br class="sp" />／Webサイト制作事業
-                <br class="sp" />／インターネットメディア運営事業
-              </p>
-            </li>
-            <li class="item">
-              <p class="label">代表者名</p>
-              <p class="TX">山田孝之</p>
-            </li>
-            <li class="item">
-              <p class="label">設立</p>
-              <p class="TX">2017年6月</p>
-            </li>
-            <li class="item">
-              <p class="label">お問合せ先</p>
-              <p class="TX">
-                当サイトに関するお問い合わせは<br
-                  class="sp"
-                />以下にお願いいたします。<br />ads@interg.co.jp<br />※メール送信の際、件名に<br
-                  class="sp"
-                />【看護師転職応援サイト宛】と入力を<br
-                  class="sp"
-                />お願いします。
-              </p>
-            </li>
-            <li class="item">
-              <p class="label"></p>
-              <p class="TX"></p>
-            </li>
-            <li class="item">
-              <p class="label"></p>
-              <p class="TX"></p>
-            </li>
-            <li class="item">
-              <p class="label"></p>
-              <p class="TX"></p>
-            </li>
-            <li class="item">
-              <p class="label"></p>
-              <p class="TX"></p>
-            </li>
-            <li class="item">
-              <p class="label"></p>
-              <p class="TX"></p>
-            </li>
-
+            <?php
+              $company_list = SCF::get('company_list');
+              foreach ($company_list as $fields) {
+                ?>
+                  <li class="item">
+                    <p class="label"><?php echo $fields['company_list_title']; ?></p>
+                    <p class="TX"><?php echo $fields['company_list_text']; ?></p>
+                  </li>
+              <?php } ?>
           </ul>
         </div>
-
-
-
         </div>
       </div>
     </section>
