9e2412b97fca80552f5e441b5f79262071ed71fe
kaku <ikumi.kaku818@gmail.com>
2024-12-04 17:39:00 +0900

    no message

--- aptitude-test-sample/js/joint.js
+++ aptitude-test-sample/js/joint.js
@@ -1,118 +1,63 @@
-// $(function () {
-//     let _click = (window.ontouchstart === undefined)? 'click' : 'touchstart';
-//     radioNumber = 0;
-//     function resultPrice() {
-//      $('.result').html(radioNumber.toLocaleString());
-//      $('.result').val(radioNumber.toLocaleString());
-//     }
-//     $('.radio').on(_click, function() {
-//      if($(this).closest('.select').find('.checked').length > 0){
-//       radioNumber = radioNumber - $(this).closest('.select').find('.checked').data('number');
-//       radioNumber = radioNumber + $(this).data('number');
-//       $(this).closest('.select').find('.checked').toggleClass('checked');
-//       $(this).toggleClass('checked');
-//      } else {
-//       radioNumber = radioNumber + $(this).data('number');
-//       $(this).toggleClass('checked');
-//      }
-//      resultPrice();
-//     })
-// });
-
 $(function () {
 	let _click = window.ontouchstart === undefined ? "click" : "touchstart";
-
-	// 合計点数を計算する関数
-	function calculateSectionTotal(section) {
-		let total = 0;
-		$(section)
-			.find(".radio.checked")
-			.each(function () {
-				total += $(this).data("number");
-			});
-		return total;
-	}
-
-	// 最も高い数字を見つける関数
-	function getHighestNumber(section) {
-		let maxNumber = -Infinity;
-		$(section)
-			.find(".radio.checked")
-			.each(function () {
-				let number = $(this).data("number");
-				if (number > maxNumber) {
-					maxNumber = number;
-				}
-			});
-		return maxNumber;
-	}
-
-	// 最も高い数字の出現回数をカウントする関数
-	function countHighestNumberOccurrences(section, highestNumber) {
-		let count = 0;
-		$(section)
-			.find(".radio.checked")
-			.each(function () {
-				if ($(this).data("number") === highestNumber) {
-					count++;
-				}
-			});
-		return count;
-	}
-
-	// 各セクションの結果を更新
-	function updateResults() {
-		$(".section-radio").each(function () {
-			let sectionId = $(this).attr("id");
-			let sectionTotal = calculateSectionTotal(this);
-			$(`#result_${sectionId}`).val(sectionTotal.toLocaleString());
-		});
-	}
+	let isFormChanged = false; // フォームの変更状態を管理
 
 	// ラジオボタン選択処理
 	$(".radio").on(_click, function () {
-		let $parentSelect = $(this).closest(".select");
-		$parentSelect.find(".checked").removeClass("checked");
+		$(this).closest(".select").find(".checked").removeClass("checked");
 		$(this).addClass("checked");
+		isFormChanged = true; // フォーム変更を記録
 		updateResults();
 	});
 
-	// 次へボタン処理
-	$(".next").off("click").on("click", function () {
-		$(this)
-			.closest("section")
-			.removeClass("active")
-			.next("section")
-			.addClass("active");
-	});
+	// リロード時の警告
+	const handleBeforeUnload = (event) => {
+		if (isFormChanged) {
+			event.preventDefault();
+			event.returnValue = ""; // デフォルトの警告を表示
+		}
+	};
+	$(window).on("beforeunload", handleBeforeUnload);
 
-	// 前へボタン処理
-	$(".prev").off("click").on("click", function () {
-		$(this)
-			.closest("section")
-			.removeClass("active")
-			.prev("section")
-			.addClass("active");
-	});
+	// 合計計算
+	const calculateSectionTotal = (section) =>
+		$(section)
+			.find(".radio.checked")
+			.toArray()
+			.reduce((sum, el) => sum + $(el).data("number"), 0);
+
+	// 各セクションの結果を更新
+	const updateResults = () =>
+		$(".section-radio").each(function () {
+			$(`#result_${this.id}`).val(calculateSectionTotal(this).toLocaleString());
+		});
 
 	// 最も高い数字が多いセクションのIDを取得
 	function getSectionWithMostHighNumbers() {
 		let highestId = null;
-		let highestMaxNumber = -Infinity;
+		let highestTotal = -Infinity;
 		let highestMaxCount = -Infinity;
+		let highestHolisticTotal = -Infinity;
 
 		$(".section-radio").each(function () {
-			let sectionId = $(this).attr("id");
-			let maxNumber = getHighestNumber(this); // セクション内の最も高い数字
-			let maxCount = countHighestNumberOccurrences(this, maxNumber); // その出現回数
+			const sectionId = this.id,
+				sectionTotal = calculateSectionTotal(this),
+				maxCount = $(this).find(".radio.checked[data-number=5]").length,
+				holisticTotal = $(this)
+					.find(".select.holistic .radio.checked")
+					.toArray()
+					.reduce((sum, el) => sum + $(el).data("number"), 0);
 
-			// 比較ロジック
 			if (
-				maxNumber > highestMaxNumber || // 最大値が高い場合
-				(maxNumber === highestMaxNumber && maxCount > highestMaxCount) // 最大値が同じ場合は出現回数で比較
+				sectionTotal > highestTotal ||
+				(sectionTotal === highestTotal && maxCount > highestMaxCount) ||
+				(sectionTotal === highestTotal &&
+					maxCount === highestMaxCount &&
+					holisticTotal > highestHolisticTotal)
 			) {
-				highestMaxNumber = maxNumber;
+				highestTotal = sectionTotal;
 				highestMaxCount = maxCount;
+				highestHolisticTotal = holisticTotal;
 				highestId = sectionId;
 			}
 		});
@@ -120,9 +65,44 @@ $(function () {
 		return highestId;
 	}
 
+	// 未選択チェック関数
+	function hasUnselectedQuestions(section) {
+		return section.find(".select").toArray().some((select) => !$(select).find("input:radio:checked").length);
+	}
+
 	// 最も高い数字が多いセクションを確認するボタン処理
-	$("#checkHighest").click(function () {
-		let highestSectionId = getSectionWithMostHighNumbers();
-		alert(`選ばれたのは: ${highestSectionId}`);
+	$("#result-page").click(function () {
+		if ($(".section-radio").toArray().some((section) => hasUnselectedQuestions($(section)))) {
+			alert("すべての項目を選択してください。");
+		} else {
+			isFormChanged = false; // 全て入力済みの場合、リロード警告を解除
+			$(window).off("beforeunload", handleBeforeUnload); // beforeunload イベント解除
+			const highestSectionId = getSectionWithMostHighNumbers();
+			window.location.href = `/index.html?section=${highestSectionId || "unknown"}`;
+		}
+	});
+
+	// URLからクエリパラメータを取得
+	const params = new URLSearchParams(window.location.search);
+	const sectionId = params.get("section");
+
+	$(".answer").removeClass("active");
+	if (sectionId) {
+		$(`#${sectionId}`).addClass("active");
+	}
+
+	// 次へボタン処理
+	$(".next").click(function () {
+		const currentSection = $(this).closest("section");
+		if (hasUnselectedQuestions(currentSection)) {
+			alert("すべての項目を選択してください。");
+		} else {
+			currentSection.removeClass("active").next("section").addClass("active");
+		}
+	});
+
+	// 前へボタン処理
+	$(".prev").click(function () {
+		$(this).closest("section").removeClass("active").prev("section").addClass("active");
 	});
 });
