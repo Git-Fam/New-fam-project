3cda78ecada130b3d9366b2fe9ed3c7612b1fd56
kaku <ikumi.kaku818@gmail.com>
2024-12-16 10:52:16 +0900

    フォント

---
+++ kbs/inc/header.php
@@ -0,0 +1,12 @@
+<header class="header">
+  <a href="<?php echo get_home_url(); ?>">home</a>
+  <a href="<?php bloginfo('url'); ?>/voi">voi</a>
+  <a href="<?php bloginfo('url'); ?>/ggg">ggg</a>
+  <a href="<?php bloginfo('url'); ?>/news">news</a>
+  <a href="<?php bloginfo('url'); ?>/blog">blog</a>
+  <a href="<?php bloginfo('url'); ?>/column">column</a>
+
+</header>
+
+<div class="whopper">
+  <?php get_template_part('./inc/kv'); ?>
---
+++ kbs/index.php
@@ -0,0 +1,6 @@
+<?php get_template_part('inchead'); ?>
+<?php get_template_part('incheader'); ?>
+
+<div class="bbb">404</div>
+
+<?php get_template_part('incfooter'); ?>
