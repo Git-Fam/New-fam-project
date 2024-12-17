83ab04e270547a4680ef4bb47a9db362edd9f56c
kaku <ikumi.kaku818@gmail.com>
2024-12-17 09:44:55 +0900

    page

--- kbs/function/common/contact.php
+++ kbs/function/common/contact.php
@@ -1,11 +1,27 @@
 <?php
 
   // コンタクトフォーム７pタグ削除
-  // add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
-  // function wpcf7_autop_return_false()
-  // {
-  //   return false;
-  // }
+  add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
+  function wpcf7_autop_return_false()
+  {
+    return false;
+  }
+
+
+//メアドの入力と確認が一致しているか
+  function wpcf7_custom_email_validation_filter( $result, $tag ) {
+    if ( 'your-email-confirm' == $tag->name ) {
+      $your_email = isset( $_POST['your-email'] ) ? trim( $_POST['your-email'] ) : '';
+      $your_email_confirm = isset( $_POST['your-email-confirm'] ) ? trim( $_POST['your-email-confirm'] ) : '';
+      if ( $your_email != $your_email_confirm ) {
+        $result->invalidate( $tag, "メールアドレスが一致しません" );
+      }
+    }
+    return $result;
+  }
+  add_filter( 'wpcf7_validate_email', 'wpcf7_custom_email_validation_filter', 20, 2 );
+  add_filter( 'wpcf7_validate_email*', 'wpcf7_custom_email_validation_filter', 20, 2 );
+
 
   //リキャプチャの読み込みを問い合わせページ、確認ページのみに限定
 // function load_recaptcha_js() {
---
+++ kbs/page-contact.php
@@ -0,0 +1,26 @@
+<?php get_template_part('./inc/head'); ?>
+<?php get_template_part('./inc/header'); ?>
+
+<div class="KV KV_contact"></div>
+
+<!-- メイン -->
+<main class="main">
+  <div class="main__contact contact_form max-size-large">
+    <div class="C_gra-vert-title">
+      <h2 class="TL">お問い合わせ</h2>
+    </div>
+
+    <div class="contact-wrap C_contact_form">
+      <p class="contact-TL">
+        ご質問やご相談がありましたら、<br class="sp">下のフォームからお気軽に<br class="sp">お問い合わせください。<br>
+        数日中に担当者よりご連絡いたします。
+      </p>
+      <?php
+        echo do_shortcode('[contact-form-7 id="07b0658" title="Contact form"]');
+      ?>
+    </div>
+  </div>
+</main>
+
+
+<?php get_template_part('./inc/footer'); ?>
