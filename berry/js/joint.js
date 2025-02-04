d400e447ebf3c99d0f88caf6cc533851ae13e470
kaku <ikumi.kaku818@gmail.com>
2025-02-05 01:26:42 +0900

    no message

---
+++ berry/js/joint.js
@@ -0,0 +1,15 @@
+$(function () {  
+
+  // 要素が画面下部に来たらshowを付与
+  $(window).scroll(function () {
+    $('.is_bounceInDown').each(function () {
+      var top_of_element = $(this).offset().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if (bottom_of_window > top_of_element) {
+        $(this).addClass('show');
+      }
+    });
+  });
+
+
+});
