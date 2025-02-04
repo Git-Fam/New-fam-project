bb30548e973a3cdd35ffbfa3cdae91c8b898c3c2
手老隆貴 <seedless0802@gmail.com>
2025-02-04 17:20:57 +0900

    no message

--- berry/js/joint.js
+++ berry/js/joint.js
@@ -2,7 +2,7 @@ $(function () {
 
   // 要素が画面下部に来たらshowを付与
   $(window).scroll(function () {
-    $('.bounceInDown').each(function () {
+    $('.is_bounceInDown').each(function () {
       var top_of_element = $(this).offset().top;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       if (bottom_of_window > top_of_element) {
