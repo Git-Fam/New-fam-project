a53ca826d8fd05973ed936bf1fcaf1230bef1f7d
手老隆貴 <seedless0802@gmail.com>
2025-02-04 17:19:11 +0900

    C_all-title

--- berry/js/joint.js
+++ berry/js/joint.js
@@ -1,30 +1,15 @@
-$(function () {   
-  // // ハンバーガーメニュー
-  //  $(".burger").on("click", function(){
-  //    $(this).toggleClass("active");
-  //    $('.menu').toggleClass("active");
-  //    $('body').toggleClass("active");
-  //  }); 
-   
-  //  // headerの下部が.mainと重なったらheaderにactiveクラスを付与
-  //    $(window).scroll(function () {
-  //      if ($(window).scrollTop() > $('.main').offset().top - 100) {
-  //        $('header').addClass('active');
-  //      } else {
-  //        $('header').removeClass('active');
-  //      }
-  //    });
+$(function () {  
 
-  // // 要素が画面下部に来たらshowを付与
-  // $(window).scroll(function () {
-  //   $('.up , .down , .right , .left , .pop').each(function () {
-  //     var top_of_element = $(this).offset().top;
-  //     var bottom_of_window = $(window).scrollTop() + $(window).height();
-  //     if (bottom_of_window > top_of_element) {
-  //       $(this).addClass('show');
-  //     }
-  //   });
-  // });
+  // 要素が画面下部に来たらshowを付与
+  $(window).scroll(function () {
+    $('.bounceInDown').each(function () {
+      var top_of_element = $(this).offset().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if (bottom_of_window > top_of_element) {
+        $(this).addClass('show');
+      }
+    });
+  });
 
 
 });
