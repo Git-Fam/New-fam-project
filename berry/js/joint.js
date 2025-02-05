601d2b173beec255984c66155a1ea669599e3623
手老隆貴 <seedless0802@gmail.com>
2025-02-06 03:34:31 +0900

    voiceコンポOK

--- berry/js/joint.js
+++ berry/js/joint.js
@@ -2,7 +2,7 @@ $(function () {
 
   // 要素が画面下部に来たらshowを付与
   $(window).scroll(function () {
-    $('.is_bounceInDown, .is_rotate-left, .is_rotate-right').each(function () {
+    $('.is_bounceInDown, .is_rotate-left, .is_rotate-right, .C_voice').each(function () {
       var top_of_element = $(this).offset().top;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       if (bottom_of_window > top_of_element) {
