e3157e1f76951807d5367a15dc597ce10224c6aa
kaku <ikumi.kaku818@gmail.com>
2025-02-05 19:13:24 +0900

    no message

--- berry/js/script.js
+++ berry/js/script.js
@@ -1,71 +1,64 @@
-  $(function () {
-    // ハンバーガーメニュー
-    $(".menu-btn").on("click", function () {
-      //  $(this).toggleClass("active");
-      //  $('.menu').toggleClass("active");
-       $('body').toggleClass("active");
-      $("header").toggleClass("active");
-    });
-  
-    // var prevScrollpos = window.pageYOffset;
-  // window.onscroll = function() {
-  //   var currentScrollpos = window.pageYOffset;
-  //   if (prevScrollpos > currentScrollpos || currentScrollpos < 450) {
-  //     document.querySelector(".header").classList.remove("active");
-  //   } else {
-  //     document.querySelector(".header").classList.add("active");
-  //   }
-  //   prevScrollpos = currentScrollpos;
-  // }
+$(function () {
+	// ハンバーガーメニュー
+	$(".menu-btn").on("click", function () {
+		//  $(this).toggleClass("active");
+		//  $('.menu').toggleClass("active");
+		$("body").toggleClass("active");
+		$("header").toggleClass("active");
+	});
 
-  // 要素が画面下部に来たらshowを付与
-  // $(window).scroll(function () {
-  //   $('.up,.down,.right,.left,.pop').each(function () {
-  //     var top_of_element = $(this).offset().top;
-  //     var bottom_of_window = $(window).scrollTop() + $(window).height();
-  //     if (bottom_of_window > top_of_element) {
-  //       $(this).addClass('show');
-  //     }
-  //   });
-  // });
+	// var prevScrollpos = window.pageYOffset;
+	// window.onscroll = function() {
+	//   var currentScrollpos = window.pageYOffset;
+	//   if (prevScrollpos > currentScrollpos || currentScrollpos < 450) {
+	//     document.querySelector(".header").classList.remove("active");
+	//   } else {
+	//     document.querySelector(".header").classList.add("active");
+	//   }
+	//   prevScrollpos = currentScrollpos;
+	// }
 
+	// 要素が画面下部に来たらshowを付与
+	$(window).scroll(function () {
+		$(".up,.down,.right,.left,.pop").each(function () {
+			var top_of_element = $(this).offset().top;
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			if (bottom_of_window > top_of_element) {
+				$(this).addClass("show");
+			}
+		});
+	});
 
-  // ローディング
-  // var loadingFinished = false;
-  // var loading = $('.loadUp,.loadDown,.loadRight,.loadLeft,.loadPop');
+	// ローディング
+	// var loadingFinished = false;
+	// var loading = $('.loadUp,.loadDown,.loadRight,.loadLeft,.loadPop');
 
-  // $(window).on('load', function () {
-  //   loading.addClass('show');
-  //   loadingFinished = true;
-  // });
-  // setTimeout(function(){
-  //   if (!loadingFinished) {
-  //     loading.addClass('show');
-  //   }
-  // }, 2000);
+	// $(window).on('load', function () {
+	//   loading.addClass('show');
+	//   loadingFinished = true;
+	// });
+	// setTimeout(function(){
+	//   if (!loadingFinished) {
+	//     loading.addClass('show');
+	//   }
+	// }, 2000);
 
+	// アコーディオン
+	// $('.js_onClick').on('click', function () {
+	//   $(this).next().slideToggle();
+	//   $(this).toggleClass('active');
+	// });
 
-  // アコーディオン
-  // $('.js_onClick').on('click', function () {
-  //   $(this).next().slideToggle();
-  //   $(this).toggleClass('active');
-  // });
-
-
+	// カウントダウンの終了日時を設定
 	// カウントダウンの終了日時を設定
 	let countdownDate = new Date("2025-04-10T00:00:00");
 
-	let x = setInterval(function () {
-		// 現在の日時を取得
+	// カウントダウン計算関数
+	function updateCountdown() {
 		let now = new Date();
-
-		// カウントダウンまでの残り時間を計算
 		let distance = countdownDate - now;
-
-		// 残り時間を日数に変換
 		let days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-		// カウントダウンが終了した場合はメッセージを表示してタイマーを停止
 		if (distance < 0) {
 			clearInterval(x);
 			$(".countdown").html("<span>終了</span>");
@@ -78,9 +71,13 @@
 				.join("");
 			$(".days").html(daysStr);
 		}
-	}, 1000);
+	}
 
+	// 初回実行
+	updateCountdown();
 
+	// 30分ごとに更新
+	let x = setInterval(updateCountdown, 1800000);
 });
 
 // 初回やリロード時は更新
