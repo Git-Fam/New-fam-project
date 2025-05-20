9a4d0e8ac4b1e70ba6b9e28f84343f9d09db9de7
kaku <ikumi.kaku818@gmail.com>
2025-05-20 15:26:32 +0900

    no message

--- In-House-Curriculum/footer.php
+++ In-House-Curriculum/footer.php
@@ -8,8 +8,6 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/loading.js"></script>
 
 <?php if (is_home()) : ?>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js"></script>
   <script type="module" src="<?php echo get_template_directory_uri(); ?>/js/cooperator-p-script.js"></script>
 <?php else : ?>
 <?php endif; ?>
