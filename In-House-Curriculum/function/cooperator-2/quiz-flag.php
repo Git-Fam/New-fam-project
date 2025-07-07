89a0187db8dba8b23c1068d596ec3ff78356ed4f
kaku <ikumi.kaku818@gmail.com>
2025-07-07 14:03:10 +0900

    no message

--- In-House-Curriculum/function/cooperator-2/quiz-flag.php
+++ In-House-Curriculum/function/cooperator-2/quiz-flag.php
@@ -56,20 +56,10 @@ function ajax_set_progress_100()
 }
 add_action('wp_ajax_set_progress_100', 'ajax_set_progress_100');
 
+
+
 function enqueue_custom_progress_script()
 {
-<<<<<<< HEAD
-    // wp_enqueue_script(
-    //     'progress-complete-script',
-    //     get_template_directory_uri() . '/js/progress-complete.js', // あなたのファイルパス
-    //     ['jquery'], // jQuery使ってないなら [] でもOK
-    //     null,
-    //     true // フッターで読み込む
-    // );
-
-    // admin-ajax.php のURLを JS に渡す
-    wp_localize_script('progress-complete-script', 'ajax_data', [
-=======
     wp_enqueue_script(
         'single-script',
         get_template_directory_uri() . '/js/single.js',
@@ -79,7 +69,6 @@ function enqueue_custom_progress_script()
     );
     
     wp_localize_script('single-script', 'ajax_data', [
->>>>>>> new2_in_house_kaku
         'ajax_url' => admin_url('admin-ajax.php'),
     ]);
 }
