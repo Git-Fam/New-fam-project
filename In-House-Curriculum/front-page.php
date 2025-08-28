eedd7eeadc36ca452eb59e6d8f476bca2e8d586c
手老隆貴 <seedless0802@gmail.com>
2025-08-28 18:01:16 +0900

    no message

--- In-House-Curriculum/front-page.php
+++ In-House-Curriculum/front-page.php
@@ -1,218 +1,199 @@
 <?php get_header(); ?>
 
 <!-- マイページボタン -->
-  <a href="<?php bloginfo('url'); ?>/login" class="my-btn">
-		<p class="btntx">マイページへ</p>
-	</a>
+<a href="<?php bloginfo('url'); ?>/login" class="my-btn">
+	<p class="btntx">マイページへ</p>
+</a>
 
-	<div class="topPage-wrapper" id="topPage-wrapper">
-		<!-- キャラ -->
-		<div class="C_character mainchara-box" id="top-mainchara">
-			<iframe
-				class="mainchara"
-				src="https://lottie.host/embed/e60cec2b-65a9-4722-99fa-d9218781a66b/TBEXhkebbF.json"
-			></iframe>
-		</div>
-		<!-- 固定ボタン -->
-		<div class="top-btn-box">
-			<a href="<?php bloginfo('url'); ?>/singin" class="btn top-btn">
-				<p class="TX">無料ではじめる</p>
-			</a>
-			<a href="<?php bloginfo('url'); ?>/aptitude-start" class="btn bottom-btn">
-				<p class="TX">適性検査</p>
-			</a>
-		</div>
-
-		<div class="topPage">
-			<!-- 1ページ -->
-			<section id="topPage01" class="topPage-bg bg01">
-				<!-- 雲 -->
-				<div class="cloud cloud01 cloudAnime"></div>
-				<!-- キャラ -->
-				<div class="C_character grayneko"></div>
-				<div class="C_character rabbit">
-					<iframe
-						src="https://lottie.host/embed/35133d55-c836-40a1-9164-f05c7a2c32a5/SLHDZUwElK.json"
-					></iframe>
-				</div>
-
-				<!-- 木 -->
-				<div class="tree tree01"></div>
-				<div class="tree tree02"></div>
-				<div class="tree tree03 pc"></div>
-
-				<!-- 草 -->
-				<div class="grass grass01"></div>
-				<div class="grass grass02"></div>
-
-				<!-- コンテンツ -->
-				<div class="content">
-					<div class="C_character bird">
-						<iframe
-							src="https://lottie.host/embed/421d3b3d-d381-49ba-b751-5d7dc96c02c8/XLrfuoTBb0.json"
-						></iframe>
-					</div>
-					<div class="TL">
-<<<<<<< HEAD
-						<h1 class="TL-text">ユメクル</h1>
-=======
-						<h1 class="TL-text">ユメクル <span>〜夢を叶えるRPG〜</span></h1>
->>>>>>> new2_in_house_Ryuuuuu
-					</div>
-					<div class="textbox">
-						<p class="textbox-top">同じ夢を持つ仲間と一緒に、<br class="sp">夢を叶えよう！</p>
-						<p class="textbox-under">
-							未経験からエンジニア・クリエイターへ！<br />
-<<<<<<< HEAD
-							全くの未経験からでも、<br class="sp">開発現場で活躍するエンジニア・クリエイターを<br class="sp">輩出してきた実績から<br />
-							エンジニア・クリエイターの卵たちの学習を通して、<br />
-							初心者にもわかりやすく構成されたカリキュラムです。
-=======
-							全くの未経験からでも、<br class="sp">挑戦を通じて現場で活躍できるスキルを獲得。<br />
-							これまで数多くのエンジニア・Webデザイナー<br class="sp">を輩出してきた実績をもとに、<br />
-							初心者でも安心して<br class="sp">成長できるカリキュラムを揃えています。
->>>>>>> new2_in_house_Ryuuuuu
-						</p>
-					</div>
-				</div>
-			</section>
-			<!-- 2ページ -->
-			<section id="topPage02" class="topPage-bg bg02">
-				<!-- 雲 -->
-				<div class="cloud cloud01 page02-cloud01 cloudAnime"></div>
-				<div class="cloud cloud01 page02-cloud02 cloudAnime02"></div>
-				<!-- キャラ -->
-				<div class="C_character mole">
-					<iframe
-						src="https://lottie.host/embed/b4994f66-3673-48c5-a9f8-a2dc72b38c6e/eWk4vLyDMj.json"
-					></iframe>
-				</div>
-
-				<!-- 木 -->
-				<!-- 草 -->
-				<div class="grass grass03"></div>
-				<!-- コンテンツ -->
-				<div class="content">
-					<div class="TL TL-bg">
-						<h2 class="TL-text">
-							<img
-								src="<?php echo get_template_directory_uri(); ?>/img/top-TL01.webp"
-								alt="コミュニティ"
-							/>を通じて<br class="sp">切磋琢磨しよう！
-						</h2>
-					</div>
-					<div class="textbox">
-						<p class="textbox-under">
-							カリキュラムの進捗を知らせる<br class="sp">タイムラインでリアクションを送り合ったり、<br />
-							進捗が近い同士の仲間たちと、<br class="sp">チャットを通じて情報共有ができるよ！<br />
-							同期のコミュニティを通じて、<br class="sp">お互いに支え合えいながら進めていこう！<br />
-						</p>
-					</div>
-					<div class="content-img content-img01"></div>
-					<div class="content-img content-img02"></div>
-				</div>
-			</section>
-			<!-- 3ページ -->
-			<section id="topPage03" class="topPage-bg bg03">
-				<!-- 雲 -->
-				<div class="cloud cloud03 cloudAnime"></div>
-				<!-- キャラ -->
-				<div class="C_character platypus01"></div>
-				<div class="C_character platypus02"></div>
-
-				<!-- 木 -->
-				<div class="tree tree04 pc"></div>
-				<!-- 草 -->
-				<!-- コンテンツ -->
-				<div class="content">
-					<div class="TL TL-bg">
-						<h2 class="TL-text">
-							成長する<img src="<?php echo get_template_directory_uri(); ?>/img/top-TL02.webp" alt="楽しさ" /> <br class="sp">を体験しよう！
-						</h2>
-					</div>
-					<div class="textbox">
-						<p class="textbox-under">
-							カリキュラムを進めてアバターを育てたり、<br class="sp">ポイントをためてランキング上位を目指したり<br />
-							ログインボーナスやクエストで<br class="sp">コインを集めてアイテムと交換したり、、、<br />
-							その他にもミニゲームやオンラインイベントなどなど<br class="sp">楽しめる機能が満載！
-						</p>
-					</div>
-					<div class="content-img content-img03"></div>
-					<div class="content-img content-img04"></div>
-				</div>
-			</section>
-			<!-- 4ページ -->
-			<section id="topPage04" class="topPage-bg bg04">
-				<!-- 雲 -->
-				<div class="cloud cloud01 cloudAnime"></div>
-				<!-- キャラ -->
-				<div class="C_character sheep">
-					<iframe
-						src="https://lottie.host/embed/8b84527e-a415-4e0b-a5cd-1de6cd3ffb1a/lxsh6nihf2.json"
-					></iframe>
-				</div>
-				<!-- 木 -->
-				<!-- 草 -->
-				<div class="grass grass04"></div>
-				<!-- コンテンツ -->
-				<div class="content">
-					<div class="TL TL-bg">
-						<h2 class="TL-text">
-							サポートを活用してぐんぐん<br class="sp">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/top-TL03.webp" alt="スキルアップ" />
-						</h2>
-					</div>
-					<div class="textbox">
-						<p class="textbox-under">
-							わからないことがあれば、いつでも現役の講師に質問して<br class="sp">すぐに解決できる環境が整っているよ！<br />
-							さらに、WEB業界の最新トレンドや<br class="sp">実践的なスキルを学べるコラムも<br class="sp">定期的に配信されているから、<br />
-							実務に役立つ知識をしっかりと身につけられる！
-						</p>
-					</div>
-					<div class="content-img content-img05"></div>
-					<div class="content-img content-img06"></div>
-					<div class="content-img content-img07"></div>
-				</div>
-			</section>
-			<!-- 5ページ -->
-			<section id="topPage05" class="topPage-bg bg05">
-				<!-- 雲 -->
-				<div class="cloud cloud01 cloudAnime"></div>
-				<div class="cloud cloud03 cloudAnime02"></div>
-				<div class="cloud balloon cloudAnime02"></div>
-				<!-- キャラ -->
-				<div class="C_character rabbit02 pc"></div>
-				<div class="C_character bird02">
-					<iframe
-						src="https://lottie.host/embed/421d3b3d-d381-49ba-b751-5d7dc96c02c8/XLrfuoTBb0.json"
-					></iframe>
-				</div>
-				<div class="C_character cat gray-cat">
-					<iframe
-						src="https://lottie.host/embed/87914d02-5736-412c-ba11-8ca247ad5cb0/qZadT1AVnK.json"
-					></iframe>
-				</div>
-				<div class="C_character cat brown-cat">
-					<iframe
-						src="https://lottie.host/embed/34849ed0-05d3-40b4-bb7b-657e4e0685c0/k9SIud6RWm.json"
-					></iframe>
-				</div>	
-
-				<!-- 木 -->
-				<!-- 草 -->
-				<div class="grass grass05 pc"></div>
-				<div class="grass grass06 pc"></div>
-				<!-- コンテンツ -->
-				<div class="content">
-					<a href="<?php bloginfo('url'); ?>/singin" class="btn top-btn">
-						<p class="TX">無料ではじめる</p>
-					</a>
-					<a href="<?php bloginfo('url'); ?>/aptitude-start" class="btn bottom-btn">
-						<p class="TX">適性検査</p>
-					</a>
-				</div>
-			</section>
-		</div>
+<div class="topPage-wrapper" id="topPage-wrapper">
+	<!-- キャラ -->
+	<div class="C_character mainchara-box" id="top-mainchara">
+		<iframe
+			class="mainchara"
+			src="https://lottie.host/embed/e60cec2b-65a9-4722-99fa-d9218781a66b/TBEXhkebbF.json"></iframe>
 	</div>
+	<!-- 固定ボタン -->
+	<div class="top-btn-box">
+		<a href="<?php bloginfo('url'); ?>/singin" class="btn top-btn">
+			<p class="TX">無料ではじめる</p>
+		</a>
+		<a href="<?php bloginfo('url'); ?>/aptitude-start" class="btn bottom-btn">
+			<p class="TX">適性検査</p>
+		</a>
+	</div>
+
+	<div class="topPage">
+		<!-- 1ページ -->
+		<section id="topPage01" class="topPage-bg bg01">
+			<!-- 雲 -->
+			<div class="cloud cloud01 cloudAnime"></div>
+			<!-- キャラ -->
+			<div class="C_character grayneko"></div>
+			<div class="C_character rabbit">
+				<iframe
+					src="https://lottie.host/embed/35133d55-c836-40a1-9164-f05c7a2c32a5/SLHDZUwElK.json"></iframe>
+			</div>
+
+			<!-- 木 -->
+			<div class="tree tree01"></div>
+			<div class="tree tree02"></div>
+			<div class="tree tree03 pc"></div>
+
+			<!-- 草 -->
+			<div class="grass grass01"></div>
+			<div class="grass grass02"></div>
+
+			<!-- コンテンツ -->
+			<div class="content">
+				<div class="C_character bird">
+					<iframe
+						src="https://lottie.host/embed/421d3b3d-d381-49ba-b751-5d7dc96c02c8/XLrfuoTBb0.json"></iframe>
+				</div>
+				<div class="TL">
+					<h1 class="TL-text">ユメクル <span>〜夢を叶えるRPG〜</span></h1>
+				</div>
+				<div class="textbox">
+					<p class="textbox-top">同じ夢を持つ仲間と一緒に、<br class="sp">夢を叶えよう！</p>
+					<p class="textbox-under">
+						未経験からエンジニア・クリエイターへ！<br />
+						全くの未経験からでも、<br class="sp">挑戦を通じて現場で活躍できるスキルを獲得。<br />
+						これまで数多くのエンジニア・Webデザイナー<br class="sp">を輩出してきた実績をもとに、<br />
+						初心者でも安心して<br class="sp">成長できるカリキュラムを揃えています。
+					</p>
+				</div>
+			</div>
+		</section>
+		<!-- 2ページ -->
+		<section id="topPage02" class="topPage-bg bg02">
+			<!-- 雲 -->
+			<div class="cloud cloud01 page02-cloud01 cloudAnime"></div>
+			<div class="cloud cloud01 page02-cloud02 cloudAnime02"></div>
+			<!-- キャラ -->
+			<div class="C_character mole">
+				<iframe
+					src="https://lottie.host/embed/b4994f66-3673-48c5-a9f8-a2dc72b38c6e/eWk4vLyDMj.json"></iframe>
+			</div>
+
+			<!-- 木 -->
+			<!-- 草 -->
+			<div class="grass grass03"></div>
+			<!-- コンテンツ -->
+			<div class="content">
+				<div class="TL TL-bg">
+					<h2 class="TL-text">
+						<img
+							src="<?php echo get_template_directory_uri(); ?>/img/top-TL01.webp"
+							alt="コミュニティ" />を通じて<br class="sp">切磋琢磨しよう！
+					</h2>
+				</div>
+				<div class="textbox">
+					<p class="textbox-under">
+						カリキュラムの進捗を知らせる<br class="sp">タイムラインでリアクションを送り合ったり、<br />
+						進捗が近い同士の仲間たちと、<br class="sp">チャットを通じて情報共有ができるよ！<br />
+						同期のコミュニティを通じて、<br class="sp">お互いに支え合えいながら進めていこう！<br />
+					</p>
+				</div>
+				<div class="content-img content-img01"></div>
+				<div class="content-img content-img02"></div>
+			</div>
+		</section>
+		<!-- 3ページ -->
+		<section id="topPage03" class="topPage-bg bg03">
+			<!-- 雲 -->
+			<div class="cloud cloud03 cloudAnime"></div>
+			<!-- キャラ -->
+			<div class="C_character platypus01"></div>
+			<div class="C_character platypus02"></div>
+
+			<!-- 木 -->
+			<div class="tree tree04 pc"></div>
+			<!-- 草 -->
+			<!-- コンテンツ -->
+			<div class="content">
+				<div class="TL TL-bg">
+					<h2 class="TL-text">
+						成長する<img src="<?php echo get_template_directory_uri(); ?>/img/top-TL02.webp" alt="楽しさ" /> <br class="sp">を体験しよう！
+					</h2>
+				</div>
+				<div class="textbox">
+					<p class="textbox-under">
+						カリキュラムを進めてアバターを育てたり、<br class="sp">ポイントをためてランキング上位を目指したり<br />
+						ログインボーナスやクエストで<br class="sp">コインを集めてアイテムと交換したり、、、<br />
+						その他にもミニゲームやオンラインイベントなどなど<br class="sp">楽しめる機能が満載！
+					</p>
+				</div>
+				<div class="content-img content-img03"></div>
+				<div class="content-img content-img04"></div>
+			</div>
+		</section>
+		<!-- 4ページ -->
+		<section id="topPage04" class="topPage-bg bg04">
+			<!-- 雲 -->
+			<div class="cloud cloud01 cloudAnime"></div>
+			<!-- キャラ -->
+			<div class="C_character sheep">
+				<iframe
+					src="https://lottie.host/embed/8b84527e-a415-4e0b-a5cd-1de6cd3ffb1a/lxsh6nihf2.json"></iframe>
+			</div>
+			<!-- 木 -->
+			<!-- 草 -->
+			<div class="grass grass04"></div>
+			<!-- コンテンツ -->
+			<div class="content">
+				<div class="TL TL-bg">
+					<h2 class="TL-text">
+						サポートを活用してぐんぐん<br class="sp">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/top-TL03.webp" alt="スキルアップ" />
+					</h2>
+				</div>
+				<div class="textbox">
+					<p class="textbox-under">
+						わからないことがあれば、いつでも現役の講師に質問して<br class="sp">すぐに解決できる環境が整っているよ！<br />
+						さらに、WEB業界の最新トレンドや<br class="sp">実践的なスキルを学べるコラムも<br class="sp">定期的に配信されているから、<br />
+						実務に役立つ知識をしっかりと身につけられる！
+					</p>
+				</div>
+				<div class="content-img content-img05"></div>
+				<div class="content-img content-img06"></div>
+				<div class="content-img content-img07"></div>
+			</div>
+		</section>
+		<!-- 5ページ -->
+		<section id="topPage05" class="topPage-bg bg05">
+			<!-- 雲 -->
+			<div class="cloud cloud01 cloudAnime"></div>
+			<div class="cloud cloud03 cloudAnime02"></div>
+			<div class="cloud balloon cloudAnime02"></div>
+			<!-- キャラ -->
+			<div class="C_character rabbit02 pc"></div>
+			<div class="C_character bird02">
+				<iframe
+					src="https://lottie.host/embed/421d3b3d-d381-49ba-b751-5d7dc96c02c8/XLrfuoTBb0.json"></iframe>
+			</div>
+			<div class="C_character cat gray-cat">
+				<iframe
+					src="https://lottie.host/embed/87914d02-5736-412c-ba11-8ca247ad5cb0/qZadT1AVnK.json"></iframe>
+			</div>
+			<div class="C_character cat brown-cat">
+				<iframe
+					src="https://lottie.host/embed/34849ed0-05d3-40b4-bb7b-657e4e0685c0/k9SIud6RWm.json"></iframe>
+			</div>
+
+			<!-- 木 -->
+			<!-- 草 -->
+			<div class="grass grass05 pc"></div>
+			<div class="grass grass06 pc"></div>
+			<!-- コンテンツ -->
+			<div class="content">
+				<a href="<?php bloginfo('url'); ?>/singin" class="btn top-btn">
+					<p class="TX">無料ではじめる</p>
+				</a>
+				<a href="<?php bloginfo('url'); ?>/aptitude-start" class="btn bottom-btn">
+					<p class="TX">適性検査</p>
+				</a>
+			</div>
+		</section>
+	</div>
+</div>
 
 <?php get_footer(); ?>
