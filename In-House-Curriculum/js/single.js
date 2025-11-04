645e8d0b70df9f1904a69db478316a8ab4dc7ee1
kaku <ikumi.kaku818@gmail.com>
2025-11-04 16:12:35 +0900

    singleページコピー禁止/右クリック禁止

--- In-House-Curriculum/js/single.js
+++ In-House-Curriculum/js/single.js
@@ -237,3 +237,14 @@ window.addEventListener("DOMContentLoaded", function () {
 		});
 	}
 });
+
+document.addEventListener('contextmenu', function(e) {
+  // 画像そのもの、または画像を含むリンクなどを検出
+  if (e.target.closest('img')) {
+    e.preventDefault();
+    return false;
+  }
+});
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
