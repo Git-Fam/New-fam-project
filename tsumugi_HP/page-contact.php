dfd384ee0b1b2a022bb2b62021189f9f387b236b
kaku <ikumi.kaku818@gmail.com>
2025-11-11 18:21:51 +0900

    WP化完了

---
+++ tsumugi_HP/page-contact.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: contact
+Template Post Type: page
+Template Path: pages/
+*/
+
+?>
+
+<?php get_template_part('./inc/head'); ?>
+
+    <main>
+
+        <div class="page contact-page">
+            <div class="contact-inner">
+                <p class="contact-TX">
+                    当クリニックにご興味を持っていただき<br class="sp">ありがとうございます。<br>
+                    ご質問のある方は下記フォームより<br class="sp">お問い合わせください。<br class="sp">「*」部分は必須入力となっております。
+                </p>
+                <?php echo do_shortcode('[contact-form-7 id="33d94c8" title="コンタクトフォーム 1"]'); ?>
+                <div class="contact-img contact-chara"></div>
+                <div class="contact-img contact-deco contact-anime delay-02"></div>
+                <div class="contact-img contact-deco02 contact-anime delay-06"></div>
+
+                </div>
+        </div>
+    </main>
+
+<?php get_template_part('./inc/footer'); ?>
