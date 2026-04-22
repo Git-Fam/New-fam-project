41423ea63925d076a59f136bd23bb8b2aab0d269
kaku <ikumi.kaku818@gmail.com>
2026-04-22 11:07:08 +0900

    プラン追加修正前

--- service/hayatoku-wifi/index.php
+++ service/hayatoku-wifi/index.php
@@ -1,379 +1,101 @@
 <?php
 
+$mode        = 'mode-blue bg-gray';
 $title       = "株式会社ConnectEarthが運営するインターネット光回線サービスギガ光";
 $description = "株式会社ConnectEarthが運営するインターネット光回線サービスギガ光。安心充実したサポートで、高速で快適なインターネットライフをご提供します。";
 $keywords    = "ギガ光,光回線,高速インターネット,格安インターネット,高速無線LAN,";
-$header_style = 'keyblue';
+$header_style = '';
 
-include "../../inc/header.php";
-include "../../inc/nav.php";
+include "../inc/header.php";
+include "../inc/nav.php";
 ?>
 
+<div class="page-header-container page-header-container--service">
+    <div class="container-fluid">
+        <img src="../img/text/text-scroll-down-white.svg" alt="Scroll down" class="text-vertical">
 
+        <div class="page-header page-header-lg">
+            <h2 class="title title-lg">
+                SERVICE
 
-<section class="section-fv section-fv--center section-fv--otokuni-mobile">
-    <img src="../../img/text/text-scroll-down.svg" alt="Scroll down" class="text-vertical hidden-xs" />
-    <img src="../../img/sp/text/text-scroll-down.svg" alt="Scroll down" class="text-vertical visible-xs" />
-    <div class="container">
-        <h2>
-            <img src="../../img/logo-otokuni-mobile.svg" alt="お得にモバイル - OTOKUNI MOBILE" class="logo" />
-            <img src="../../img/logo-otokuni-mobile.svg" alt="お得にモバイル - OTOKUNI MOBILE" class="logo-sm" />
-        </h2>
+                <small>
+                    取扱商品
+                </small>
+            </h2>
+
+            <img src="../img/text/text-grow-support-white.svg" alt="ConnectEarth" width="108" class="text-img" />
+        </div>
     </div>
-</section>
-
-<div class="section-features-container">
-    <section class="section-features">
-
-        <h4 class="text-img-vertical text-left">
-            <img src="../../img/text/text-otokuni-mobile-1.svg" alt="OTOKUNI MOBILE - 1" />
-        </h4>
-
-        <div class="container-fluid">
-            <div class="media media-title">
-                <div class="media-body">
-                    <h3 class="media-heading">
-                        プラン紹介
-                    </h3>
-                </div>
-            </div>
-
-            <table class="table table-gray table-bordered">
-                <thead>
-                    <tr>
-                        <th>プラン名</th>
-                        <th>通信料</th>
-                        <th>端末割賦</th>
-                        <th>割引</th>
-                        <th>合計</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>はやトクWi-Fi<br>MACALOON10GB</td>
-                        <td>2,980円(税抜き)</td>
-                        <td>1,500円(税抜き)</td>
-                        <td>-2,500円(税抜き)</td>
-                        <td>1,980円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td>はやトクWi-Fi<br>MACALOON20GB</td>
-                        <td>4,480円(税抜き)</td>
-                        <td>1,500円(税抜き)</td>
-                        <td>-2,600円(税抜き)</td>
-                        <td>3,380円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td>はやトクWi-Fi<br>MACALOON50GB</td>
-                        <td>6,980円(税抜き)</td>
-                        <td>1,500円(税抜き)</td>
-                        <td>-4,600円(税抜き)</td>
-                        <td>3,880円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td>はやトクWi-Fi<br>MACALOON100GB</td>
-                        <td>7,980円(税抜き)</td>
-                        <td>1,500円(税抜き)</td>
-                        <td>-4,500円(税抜き)</td>
-                        <td>4,980円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td rowspan="2">はやトクWi-Fi<br>MACALOONCP100GB</td>
-                        <td>1年目<br>7,980円(税抜き)</td>
-                        <td>1,500円(税抜き)</td>
-                        <td>-6,900円(税抜き)</td>
-                        <td>2,580円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td>2～3年目<br>7,980円(税抜き)</td>
-                        <td>1,500円(税抜き)</td>
-                        <td>-4,500円(税抜き)</td>
-                        <td>4,980円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td>はやトクWi-Fi<br>MACALOON150GB</td>
-                        <td>8,980円(税抜き)</td>
-                        <td>1,500円(税抜き)</td>
-                        <td>-5,280円(税抜き)</td>
-                        <td>5,200円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td>はやトクWi-Fi<br>MACALOON200GB</td>
-                        <td>9,980円(税抜き)</td>
-                        <td>1,500円(税抜き)</td>
-                        <td>-5,960円(税抜き)</td>
-                        <td>5,520円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td>はやトクWi-Fi<br>MACALOON100GB HOME</td>
-                        <td>7,980円(税抜き)</td>
-                        <td>2,050円(税抜き)</td>
-                        <td>-5,050円(税抜き)</td>
-                        <td>4,980円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td rowspan="2">はやトクWi-Fi<br>MACALOONCP100GB<br>HOME</td>
-                        <td>1年目<br>7,980円(税抜き)</td>
-                        <td>2,050円(税抜き)</td>
-                        <td>-7,450円(税抜き)</td>
-                        <td>2,580円(税抜き)</td>
-                    </tr>
-                    <tr>
-                        <td>2～3年目<br>7,980円(税抜き)</td>
-                        <td>2,050円(税抜き)</td>
-                        <td>-5,050円(税抜き)</td>
-                        <td>4,980円(税抜き)</td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <p class="note">
-                ※3年契約となります
-            </p>
-
-
-
-            <!-- <div class="media media-title ml-0 mr-0 max-width-1280">
-                <div class="media-body">
-                    <h3 class="media-heading">
-                        請求先名称変更に関するお知らせ
-                    </h3>
-                </div>
-            </div>
-
-            <p>
-                引落時の名称<br>
-                現状：　　ＧｒｏｗＳｕｐｐｏｒｔ<br>
-                変更後：　はやトクWi-Fi<br><br>
-                クレジットカード会社にて順次変更のご対応をいただいておりますので<br>
-                ご利用いただいておりますクレジットカードによって変更のタイミングが異なります。
-            </p> -->
-
-            <div class="media media-title">
-                <div class="media-body">
-                    <h3 class="media-heading">
-                        特定商取引法に基づく表記
-                    </h3>
-                </div>
-            </div>
-
-            <table class="table table-gray2 table-bordered">
-                <tbody>
-                    <tr>
-                        <th class="col-1">
-                            事業者名
-                        </th>
-                        <td>
-                            株式会社ConnectEarth
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            本店所在地
-                        </th>
-                        <td>
-                            北海道札幌市中央区北2条東1丁目5-2　E-horizon北2条　2階
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            連絡先
-                        </th>
-                        <td>
-                            0800-888-7279
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            電気通信事業者届出番号
-                        </th>
-                        <td>
-                            J-06-876
-                        </td>
-                    </tr>
-            
-                    <tr>
-                        <th>
-                            商品等の販売価格
-                        </th>
-                        <td>
-                            上部プラン紹介に記載
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            商品代金の以外の必要料金
-                        </th>
-                        <td>
-                            【送料】無料<br>
-                            【事務手数料】4,500円(税込4,950円)<br>
-                            【任意】WiFi安心サービス600円/月（税込660円）
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            ご契約可能数
-                        </th>
-                        <td>
-                            1名義につき2台まで
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            代金の支払い時期
-                        </th>
-                        <td>
-                            クレジットカード会社の期限に準ずる
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            商品等の引き渡し時期
-                        </th>
-                        <td>
-                            ご契約完了後、最短2営業日～発送致します
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            返品の可否と条件
-                        </th>
-                        <td>
-                            商品・契約書面を受け取った日を含む8日以内に事前の電話申告にて返品が可能です。また、商品・契約書面を受け取った日を含む8日以内に一式を当社指定住所へ返送する必要があります。<br>
-                            以下の場合、返品不可または別途費用が発生する場合が御座いますのでご注意ください。<br>
-                            ・弊社への申告期限、商品一式の返送期限を過ぎていた場合<br>
-                            ・返送商品に(外箱含む)一式に欠品・破損・故障がある場合の機器損害金<br>
-                            ・商品(外箱含む)に封印シールが張られている場合、シールを剥がしたものを返品する場合
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <br><br>
-            <dl class="dl-list-indent dl-title-icon title-note">
-                <dd>
-                    <h3 class="title">
-                        注意事項
-                    </h3>
-                </dd>
-            </dl>
-            <ul class="list-inline list-link-external">
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-10GB.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOON10GB
-                    </a>
-                </li>
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-20GB.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOON20GB
-                    </a>
-                </li>
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-50GB.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOON50GB
-                    </a>
-                </li>
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-100GB.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOON100GB
-                    </a>
-                </li>
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-CP-100GB.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOONCP100GB
-                    </a>
-                </li>
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-150GB.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOON150GB
-                    </a>
-                </li>
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-200GB.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOON200GB
-                    </a>
-                </li>
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-100GB-HOME.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOON100GB HOME
-                    </a>
-                </li>
-                <li>
-                    <a href="../../pdf/hayatoku-wifi/H-W-M-CP-100GB-HOME.pdf" target="_blank" class="underline" target="_blank">
-                        はやトクWi-Fi MACALOONCP100GB HOME
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </section>
 </div>
 
-<div class="section-contact-container">
-    <section class="section-contact">
-        <h4 class="text-img-vertical text-left">
-            <img src="../../img/text/text-otokuni-mobile-2.svg" alt="OTOKUNI MOBILE - 2" />
-        </h4>
+<div class="section-single-service-container">
+    <section class="single-section-service">
+        <h3 class="text-img-vertical text-left">
+            <img src="../img/text/text-grow-support-vertical.svg" alt="ConnectEarth" width="13" />
+        </h3>
 
-        <div class="container-fluid">
-            <div class="media media-title media-title-section">
-                <div class="media-body">
-                    <h5 class="media-heading">
+        <div class="row">
+            <!-- <div class="col-sm-4 px-lg-5">
+                <a href="giga-hikari" class="service-link-card service-link-card--green">
+                    <img src="../img/logo-giga-hikari.svg" alt="" class="service-link-card__image">
+                    <p class="service-link-card__text">ギガ光ページへ</p>
+                </a>
+                <h4 class="service-name service-name--green">ギガ光とは</h4>
+                <p class="service-text service-text--green">下り最大2Gbpsの高速インターネット<br>無制限のWiFi +ウィルス対策も標準装備</p>
+            </div> -->
+            <!-- <div class="col-sm-4 px-lg-5">
+                <a href="otoku-denki" class="service-link-card service-link-card--orange">
+                    <img src="../img/logo-otoku-denki.svg" alt="" class="service-link-card__image">
+                    <p class="service-link-card__text">おとくに電気プランページへ</p>
+                </a>
+                <h4 class="service-name service-name--orange">おとくに電気プランとは</h4>
+                <p class="service-text service-text--orange">日々の生活に欠かせない電気料金を最大5%割引<br>電気の品質 変わらず 毎月おとく</p>
+            </div> -->
+            <!-- col-sm-4排除 -->
+            <div class="px-lg-5">
+                <a href="/service/hayatoku-wifi/" class="service-link-card service-link-card--blue">
+                    <img src="../img/logo-otokuni-mobile.svg" alt="" class="service-link-card__image">
+                    <p class="service-link-card__text">はやトクWi-Fiページへ</p>
+                </a>
+                <h4 class="service-name service-name--blue">はやトクWi-Fiとは</h4>
+                <p class="service-text service-text--blue">工事不要！持ち運び可能なWiFiルーター<br>利用頻度に合わせたおトクなサービス</p>
+            </div>
+            <!-- <div class="px-lg-5 pt-lg-3">
+                <div class="simple-service-link">
+                    <h4 class="service-name service-name--dark-blue">医療サポートパック</h4>
+                    <p class="service-text">医師・保健師・看護師等による健康・医療・介護・育児・メンタルヘルスの電話相談、夜間・休日の医療機関情報の情報提供・介護などシルバー情報の情報提供が受けられるサービスとなります。</p>
+                    <a href="/service/medical-support-pack/" class="btn btn-dark-blue btn-block arrow-right">
                         お問い合わせ
-                    </h5>
-                </div>
-            </div>
-
-            <div class="row row-25-lg">
-
-                <div class="col-base col-md-435">
-                    <div class="well well-border-gray" data-mh="well">
-                        <h6 class="title-well title-blue-sm" data-mh="well-title">
-                            入力フォームからお申し込み
-                        </h6>
-
-                        <div data-mh="text-well">
-                            はやトクWi-Fiに関するお申し込みは<br />
-                            こちらのフォームをご利用ください。
-                        </div>
-
-
-                        <div class="container-btn">
-                            <a href="../../contact/" class="btn btn-blue btn-block arrow-right">
-                                お問い合わせ
-                            </a>
-                        </div>
-                    </div>
+                    </a>
                 </div>
 
-                <div class="col-base col-md-475">
-                    <div class="well well-border-gray well-tell" data-mh="well">
-                        <h6 class="title-well title-blue-sm" data-mh="well-title">
-                            お電話からお問い合わせ
-                        </h6>
-
-                        <div data-mh="text-well">
-                            お急ぎの場合はお電話にてお問い合わせください。<br />
-                            サポート営業時間<br />
-                            月曜日～日曜日（※定休日なし） 12:00～18:00
-                        </div>
-
-                        <dl class="dl-list-indent dl-tel">
-                            <dt>
-                                <img src="../../img/icon/phone-green.svg" alt="" class="icon" />
-                            </dt>
-                            <dd>
-                                <a href="tel:0800-888-7279" class="hover-img">
-                                    <img src="../../img/tel-otokuni-mobile.svg" alt="0800-888-7279" width="200" class="img-responsive tel" />
-                                </a>
-                            </dd>
-                        </dl>
-                    </div>
-                </div>
-            </div>
+            </div> -->
         </div>
     </section>
 </div>
+
+<div class="section-contact-block-container">
+    <section class="section-contact-block">
+        <div class="container-fluid">
+            <div class="content">
+                <h4 class="title-section">
+                    <span>CONTACT</span>
+                    <small>お問い合わせ</small>
+                </h4>
+                <p class="text visible-lg visible-md">ConnectEarthに関する各種お問い合わせ・ご要望は<br>こちらのフォームをご利用ください。</p>
+            </div>
+            <a href="/contact/" class="link">
+                <p class="link-text">
+                    <span>お問い合わせへ</span>
+                </p>
+            </a>
+            <p class="text visible-sm visible-xs">ConnectEarthに関する各種お問い合わせ・ご要望はこちらのフォームをご利用ください。</p>
+        </div>
+    </section>
+</div>
+
+
 
 <?php
-include "../../inc/footer.php";
+include "../inc/footer.php";
 ?>
