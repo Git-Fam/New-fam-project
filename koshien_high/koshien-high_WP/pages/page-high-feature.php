db63e84de870e25774bbb1cb733ce26453c93c2b
手老隆貴 <seedless0802@gmail.com>
2026-07-13 10:55:36 +0900

    no message

--- koshien_high/koshien-high_WP/pages/page-high-feature.php
+++ koshien_high/koshien-high_WP/pages/page-high-feature.php
@@ -12,20 +12,134 @@ Template Path: pages/
 
 <main class="page page--high-feature page--high-all">
 
-
   <section class="high-feature-kv">
     <div class="high-feature-kv-bg">
       <picture>
-        <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-kv-bg-psp.webp" type="image/svg+xml">
+        <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-kv-bg-sp.webp" type="image/svg+xml">
         <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-kv-bg-pc.webp" alt="">
+      </picture>
+    </div>
+    <h2 class="TL">
+      <picture>
+        <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-kv-ttl-sp.svg" type="image/svg+xml">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-kv-ttl-pc.svg" alt="発見！学びのヒミツ 学びの特色">
+      </picture>
+    </h2>
+  </section>
+
+  <section class="high-feature-content">
+    <div class="high-feature-content-inr">
+      <div class="content-item js-fade">
+        <div class="content-item-img">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-img-01.webp" alt="">
+        </div>
+        <div class="content-item-txt">
+          <p class="TG">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-tg-01.svg" alt="POINT 01">
+          </p>
+          <h3 class="TL">
+            <picture>
+              <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-ttl-01-sp.svg" type="image/svg+xml">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-ttl-01-pc.svg" alt="専用のブースがあるから質問がしやすい！">
+            </picture>
+          </h3>
+          <p class="TX">
+            ダミー「one-on-one デスク」で、日々の学習は<br class="pc">
+            もちろん、質問対応も可能なデスクを常設していま<br class="pc">
+            す。自習室での学習と併せて状況によって使い分け<br class="pc">
+            ることができます。周りを気にせず自分のペースで<br class="pc">
+            集中して学習・質問もできるので好評です。
+          </p>
+        </div>
+      </div>
+      <div class="content-item js-fade">
+        <div class="content-item-img">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-img-02.webp" alt="">
+        </div>
+        <div class="content-item-txt">
+          <p class="TG">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-tg-02.svg" alt="POINT 02">
+          </p>
+          <h3 class="TL">
+            <picture>
+              <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-ttl-02-sp.svg" type="image/svg+xml">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-ttl-02-pc.svg" alt="自習室には32個の個別ブースを完備！">
+            </picture>
+          </h3>
+          <p class="TX">
+            ダミー定期試験や入学試験前など集中して学習<br class="pc">
+            したい時に利用している生徒が多く、静かで落<br class="pc">
+            ち着いた学習環境を提供しています。
+          </p>
+        </div>
+      </div>
+      <div class="content-item js-fade">
+        <div class="content-item-img">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-img-03.webp" alt="">
+        </div>
+        <div class="content-item-txt">
+          <p class="TG">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-tg-03.svg" alt="POINT 03">
+          </p>
+          <h3 class="TL">
+            <picture>
+              <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-ttl-03-sp.svg" type="image/svg+xml">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-ttl-03-pc.svg" alt="専属のカウンセラーがいるから安心！">
+            </picture>
+          </h3>
+          <p class="TX">
+            ダミー学校生活の中で生まれる様々な悩みや不安を相談で<br class="pc">
+            きるカウンセリングルームを設置。専属のスクールカウン<br class="pc">
+            セラー（ 週２日常駐）が生徒や保護者の皆様の相談を受<br class="pc">
+            け付けています。
+          </p>
+        </div>
+      </div>
+      <div class="content-item js-fade">
+        <div class="content-item-img">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-img-04.webp" alt="">
+        </div>
+        <div class="content-item-txt">
+          <p class="TG">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-tg-04.svg" alt="POINT 04">
+          </p>
+          <h3 class="TL">
+            <picture>
+              <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-ttl-04-sp.svg" type="image/svg+xml">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-content-item-ttl-04-pc.svg" alt="頑張りたい生徒を積極的に伸ばすカリキュラム！">
+            </picture>
+          </h3>
+          <p class="TX">
+            ダミー総合演習等を通じて、勉強をもっと頑張りたい<br class="pc">
+            生徒を伸ばせるカリキュラムがあります。ダミー総合<br class="pc">
+            演習等を通じて、勉強をもっと頑張りたい生徒を伸ば<br class="pc">
+            せるカリキュラムがあります。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="high-feature-number">
+    <h3 class="TL js-fade">
+      <picture>
+        <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-number-ttl-sp.svg" type="image/svg+xml">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-number-ttl-pc.svg" alt="数字ものぞいてみる？ 数字で見る甲子園学院高等学校">
+      </picture>
+    </h3>
+    <div class="high-feature-number-content js-fade">
+      <picture>
+        <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-number-contents-sp.webp" type="image/svg+xml">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/high-feature/high-feature-number-contents-pc.webp" alt="">
       </picture>
     </div>
   </section>
 
+  <section class="high-feature-more_check">
+    <?php get_template_part('./inc/high_C_more_check'); ?>
+  </section>
 
 
-
-  
 </main>
 
 
