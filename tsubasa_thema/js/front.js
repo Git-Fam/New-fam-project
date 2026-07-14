6ed318b0ecf67d3f190077c531165ae791a67478
手老隆貴 <seedless0802@gmail.com>
2026-07-14 10:25:17 +0900

    no message

---
+++ tsubasa_thema/js/front.js
@@ -0,0 +1,72 @@
+$(function () {
+
+
+	// KVスクロールアニメーション（枝・芝を外側へ）
+	if (window.gsap && window.ScrollTrigger) {
+		gsap.registerPlugin(ScrollTrigger);
+
+		const kvBranchTl = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".front-kv",
+				start: "50% top",
+				end: "100% top", // front-kvは100vhなので約50vhスクロールで完了
+				scrub: true,
+			},
+		});
+
+		kvBranchTl
+			.to(".front-kv-branch-l", { xPercent: -100, scale: 1.3, transformOrigin: "left top", ease: "power2.in" }, 0)
+			.to(".front-kv-branch-r", { xPercent: 100, scale: 1.3, transformOrigin: "right top", ease: "power2.in" }, 0)
+			.to(".front-kv-lawn", { yPercent: 100, scale: 1.3, transformOrigin: "center bottom", ease: "power2.in" }, 0);
+
+
+		const kvContentsTl = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".front-kv",
+				start: "60% top",
+				end: "130% top", // front-kvは100vhなので約50vhスクロールで完了
+				scrub: true,
+			},
+		});
+
+		kvContentsTl
+			.to(".front-kv-contents", { scale: 2, transformOrigin: "center top", opacity: 0, ease: "power2.in" }, 0)
+
+		const kvBgTl = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".front-kv",
+				start: "60% top",
+				end: "140% top", // front-kvは100vhなので約50vhスクロールで完了
+				scrub: true,
+			},
+		});
+
+		kvBgTl
+			.to(".front-kv-bg", { scale: 2, transformOrigin: "center top", opacity: 0, ease: "power2.in" }, 0)
+
+		kvContentsTl
+			.to(".front-kv-contents", { scale: 2, transformOrigin: "center top", opacity: 0, ease: "power2.in" }, 0)
+
+		const kvWhiteBgTl = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".front-kv",
+				start: "180% top",
+				end: "230% top", // front-kvは100vhなので約50vhスクロールで完了
+				scrub: true,
+			},
+		});
+		kvWhiteBgTl
+			.to(".front-kv-white-bg", { opacity: 0, ease: "sine.out" }, 0)
+	}
+
+	// ヘッダーの表示制御（トップから210vhを境に切り替え）
+	$(window).on("scroll", function () {
+		if ($(this).scrollTop() > window.innerHeight * 2.1) {
+			$(".front-hidden").removeClass("is-hidden");
+		} else {
+			$(".front-hidden").addClass("is-hidden");
+		}
+	});
+
+
+});
---
+++ tsubasa_thema/js/script.js
@@ -0,0 +1,35 @@
+$(function () {
+	// ハンバーガーメニュー
+	$(".header-btn").on("click", function () {
+		$('body').toggleClass("is-active");
+	});
+
+	// 	メニュー内のアコーディオン
+	$(".main-list-item-accordion").on("click", function () {
+		$(this).toggleClass("is-active");
+	});
+
+
+	// 要素が画面下部に来たらshowを付与
+	// $(window).scroll(function () {
+	//   $('.up,.roll').each(function () {
+	//     var top_of_element = $(this).offset().top;
+	//     var bottom_of_window = $(window).scrollTop() + $(window).height();
+	//     if (bottom_of_window > top_of_element) {
+	//       $(this).addClass('show');
+	//     }
+	//   });
+	// });
+	// ローディング
+	// var loadingFinished = false;
+	// var loading = $('.loadUp');
+	// $(window).on('load', function () {
+	//   loading.addClass('show');
+	//   loadingFinished = true;
+	// });
+	// setTimeout(function(){
+	//   if (!loadingFinished) {
+	//     loading.addClass('show');
+	//   }
+	// }, 2000);
+});
