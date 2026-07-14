911ca11e9ffa24e975525344ecf9024adf67223b
kaku <ikumi.kaku818@gmail.com>
2026-07-14 12:09:14 +0900

    no message

--- koshien_high/koshien-high_WP/pages/page-junior-club.php
+++ koshien_high/koshien-high_WP/pages/page-junior-club.php
@@ -14,7 +14,7 @@ Template Path: pages/
 <section class="high-club-kv">
     <div class="high-club-kv-bg">
       <picture>
-        <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/junior-club/junior-club-kv-bg-psp.webp" type="image/svg+xml">
+        <source media="(max-width:767px)" srcset="<?php echo get_template_directory_uri(); ?>/img/junior-club/junior-club-kv-bg-sp.webp" type="image/svg+xml">
         <img src="<?php echo get_template_directory_uri(); ?>/img/junior-club/junior-club-kv-bg-pc.webp" alt="">
       </picture>
     </div>
