76c73a042309b737876527756ec0ff475a2acae7
手老隆貴 <seedless0802@gmail.com>
2026-07-17 14:27:51 +0900

    フロント完成

--- tsubasa_thema/js/about-surgery.js
+++ tsubasa_thema/js/about-surgery.js
@@ -1,1 +1,23 @@
-$(function () {});
+$(function () {
+
+	if (window.Swiper) {
+		new Swiper(".type-swiper", {
+			slidesPerView: 1,
+			centeredSlides: true,
+			loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+			navigation: {
+				prevEl: ".type-swiper-prev",
+				nextEl: ".type-swiper-next",
+			},
+			pagination: {
+				el: ".type-swiper-pagination",
+				clickable: true,
+			},
+		});
+	}
+
+});
--- tsubasa_thema/js/faq.js
+++ tsubasa_thema/js/faq.js
@@ -1,1 +1,6 @@
-$(function () {});
+$(function () {
+
+  $(".faq-contents-inr-item .item-wrap .item .item-q").on("click", function () {
+    $(this).parent().toggleClass("is-active");
+  });
+});
