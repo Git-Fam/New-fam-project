7b1f1d6a6061dff9759b7677995d63ec54169b40
kaku <ikumi.kaku818@gmail.com>
2026-08-17 15:41:01 +0900

    no message

--- AITEC_LP/js/script.js
+++ AITEC_LP/js/script.js
@@ -33,27 +33,27 @@ $(function () {
     //   prevScrollpos = currentScrollpos;
     // }
     // 要素が画面下部に来たらshowを付与
-    // $(window).scroll(function () {
-    //   $('.up,.down,.right,.left,.pop').each(function () {
-    //     var top_of_element = $(this).offset().top;
-    //     var bottom_of_window = $(window).scrollTop() + $(window).height();
-    //     if (bottom_of_window > top_of_element) {
-    //       $(this).addClass('show');
-    //     }
-    //   });
-    // });
+    $(window).scroll(function () {
+      $('.up,.down,.right,.left,.pop').each(function () {
+        var top_of_element = $(this).offset().top;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > top_of_element) {
+          $(this).addClass('show');
+        }
+      });
+    });
     // ローディング
-    // var loadingFinished = false;
-    // var loading = $('.loadUp,.loadDown,.loadRight,.loadLeft,.loadPop');
-    // $(window).on('load', function () {
-    //   loading.addClass('show');
-    //   loadingFinished = true;
-    // });
-    // setTimeout(function(){
-    //   if (!loadingFinished) {
-    //     loading.addClass('show');
-    //   }
-    // }, 2000);
+    var loadingFinished = false;
+    var loading = $('.loadUp,.loadDown,.loadRight,.loadLeft,.loadPop');
+    $(window).on('load', function () {
+      loading.addClass('show');
+      loadingFinished = true;
+    });
+    setTimeout(function(){
+      if (!loadingFinished) {
+        loading.addClass('show');
+      }
+    }, 2000);
     // FAQ：アコーディオン開閉
     $('.js-faq-toggle').on('click', function () {
         var $item = $(this).closest('.faq__item');
